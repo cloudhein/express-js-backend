949049a29e7fb8e7eaff5fb1bb6e5d21e8656ca9
cloudhein <cloudhein@users.noreply.github.com>
2025-04-02 18:05:06 +0000

    Updated from ref: a3e5a28cb07d5efb66f0fe357c39ae58ff7db69e

--- express-backend/index.js
+++ express-backend/index.js
@@ -6,11 +6,11 @@ const port = 3000;
 app.use(cors());
 
 app.get("/api/v1/hello", (req, res) => {
-     res.json({ message: "nanda" });
+  res.json({ message: "nanda" });
 });
 
 const server = app.listen(port, () => {
-     console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
 
 module.exports = { app, server };
